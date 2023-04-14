88b482dee51b67eb7182a26f625e86b870289fe6
RC-Regalado <ruben.alekey@gmail.com>
2023-04-14 15:33:58 -0600

    Pasarela de pagos
    Signed-off-by: RC-Regalado <ruben.alekey@gmail.com>

--- functions.php
+++ functions.php
@@ -46,12 +46,12 @@ function custom_login($content)
 <?php
             wp_login_form(
             array(
-                                'echo' => true ,
-                                'redirect'       => (is_ssl() ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] .'/' ,
-                                       'label_username' => __('Nombre de usuario '),
-                                       'label_password' => __('Contraseña'),
-                                       'label_remember' => __('Recuérdame')
-                            )
+                                    'echo' => true ,
+                                    'redirect'       => (is_ssl() ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] .'/' ,
+                                           'label_username' => __('Nombre de usuario '),
+                                           'label_password' => __('Contraseña'),
+                                           'label_remember' => __('Recuérdame')
+                                )
         );
             ?>
             </div>
